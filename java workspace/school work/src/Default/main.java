6a6a8bf9decf1ff3b72060901f001e247d59adc0
mimramon <66071997+mimramon@users.noreply.github.com>
2020-10-06 21:48:16 +0100

    Add files via upload

---
+++ java workspace/school work/src/Default/main.java
@@ -0,0 +1,16 @@
+package Default;
+
+import java.util.*;
+
+public class main //game manager class
+{
+    public static Scanner scan= new Scanner(System.in);
+    public static String proceed;
+    public static void main(String[] args)  //start function
+    {
+        System.out.println("Welcome to zombie apocalypse.\nPress enter to continue.");
+        proceed = scan.next();
+        
+    }
+    
+}
---
+++ java workspace/school work/src/Default/settlement.java
@@ -0,0 +1,17 @@
+package Default;
+
+public class settlement 
+{
+    //#region BUILDINGS
+    public int houses;
+    public boolean workshop;
+    //#endregion
+
+    //#region RESOURCES
+    public int wood;
+    public int metal;
+    public int food;
+    public int meds;
+    public int ammo;
+    //#endregion
+}
