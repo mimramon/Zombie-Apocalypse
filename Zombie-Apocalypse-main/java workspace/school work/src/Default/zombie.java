3add6e16706cc2a40877d6ef17925d2f30ae8da4
obeenham <obeenham@gmail.com>
2020-10-07 17:10:39 +0100

    Revert "Update zombie.java"
    This reverts commit 7369b98e9832e8f42b0215c5a5ade519927af297.

--- Zombie-Apocalypse-main/java workspace/school work/src/Default/zombie.java
+++ Zombie-Apocalypse-main/java workspace/school work/src/Default/zombie.java
@@ -1,38 +1,6 @@
 package Default;
 
-// Importing
-import java.util.*;
-
 public class zombie 
 {
-	// VARIABLES - Methods
-	public Random rand = new Random();
-
-	// VARIABLES - Zombie Types
-	static public final String[] zombieTypes = {"Charger", "Volatile Charger", "Toxic"};
-	
-	// VARIABLES - Zombie statistics 
-    public int combatStrength;
-
-    // VARIABLES - Misc
-    public String role;
     
-    public void setStats ()
-    {
-    	switch (role) 
-    	{
-    		case "Charger":
-    			combatStrength = 3;
-    			break;
-    		case "Volatile Charger":
-    			combatStrength = 4;
-    			break;
-    		case "Toxic":
-    			combatStrength = 5;
-    			break;
-    		default:
-    			System.out.println("There was an error setting the stats for this zombie");
-    			break;
-    	}
-    }
 }
