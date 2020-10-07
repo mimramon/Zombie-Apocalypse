82d2e39667054d254c0e4e0094863984783fdf67
obeenham <obeenham@gmail.com>
2020-10-07 17:25:14 +0100

    Update main.java
    Tidied up

--- Zombie-Apocalypse-main/java workspace/school work/src/Default/main.java
+++ Zombie-Apocalypse-main/java workspace/school work/src/Default/main.java
@@ -1,8 +1,9 @@
 package Default;
 
+// Imports
 import java.util.*;
 
-public class main //game manager class
+public class main // Game manager class
 {
     public static Scanner scan = new Scanner(System.in);
     public static Random rand = new Random();
@@ -13,59 +14,68 @@ public class main //game manager class
     public static int event;
     
     
-    public static void main(String[] args) //start function
+    public static void main(String[] args) // Start function
     {
         System.out.println("Welcome to zombie apocalypse.\nPress enter to continue.");
         proceed = scan.nextLine();
         
-        //creates starting party
+        // Sreates starting party
         PARTY[0] = new survivor();
-        PARTY[0].role ="hunter";
+        PARTY[0].role ="Hunter";
         PARTY[0].setStats();
         PARTY[1] = new survivor();
-        PARTY[1].role ="hunter";
+        PARTY[1].role ="Hunter";
         PARTY[1].setStats();
         PARTY[2] = new survivor();
-        PARTY[2].role ="builder";
+        PARTY[2].role ="Builder";
         PARTY[2].setStats();
         PARTY[3] = new survivor();
-        PARTY[3].role ="medic";
+        PARTY[3].role ="Medic";
         PARTY[3].setStats();
         
         while(round <= 100 && !isDead)
         {
-        	System.out.println("you are now startring round "+round);
-        	
+        	System.out.println("\nYou are now startring round "+round);
+			
+			// In-game event handler
         	event = rand.nextInt(6);
         	
         	switch (event)
         	{
-        	case 0:
-        		System.out.println("zombie");
-        		break;
-        	case 1:
-        		System.out.println("resource");
-        		break;
-        	case 2:
-        		System.out.println("zombie");
-        		break;
-        	case 3:
-        		System.out.println("food");
-        		break;
-        	case 4:
-        		System.out.println("nothing");
-        		break;
-        	case 5:
-        		System.out.println("survivor");
-        		break;
-        	default:
-        		System.out.println("error starting round");
-        		break;
+				// Zombie event
+        		case 0:
+        			System.out.println("Zombie");
+					break;
+				// Resource event
+        		case 1:
+        			System.out.println("Resource");
+					break;
+				// Another zombie event (To increase the chances of it occuring)
+        		case 2:
+        			System.out.println("Zombie");
+					break;
+				// Food event
+        		case 3:
+        			System.out.println("Food");
+					break;
+				// Nothing happens
+        		case 4:
+        			System.out.println("Nothing");
+					break;
+				// Survivor event
+        		case 5:
+        			System.out.println("Survivor");
+					break;
+				// Error
+        		default:
+        			System.out.println("There was an error starting the round");
+        			break;
         	}
-        	
+			
+			// Idle tasks thhat occur every round
         	for (int i = 0; i < PARTY.length ; i++)
         	{
-        		System.out.println(i);
+        		//System.out.println("IDLE TASK GOES HERE");
         		//do the idle actions of the party member
         	}
         
