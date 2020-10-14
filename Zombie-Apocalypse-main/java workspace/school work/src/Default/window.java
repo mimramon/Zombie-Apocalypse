e1df3814a0982d07fc7298f2d6903d24465c9ab0
obeenham <obeenham@gmail.com>
2020-10-14 16:12:47 +0100

    Update window.java

--- Zombie-Apocalypse-main/java workspace/school work/src/Default/window.java
+++ Zombie-Apocalypse-main/java workspace/school work/src/Default/window.java
@@ -9,12 +9,16 @@ public class window
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Make the window exit when X button pressed
 
         JButton loadButton = new JButton("Load Game"); // Make the saveButton
-        loadButton.setBounds(20,50,100,40); // IN ORDER: X Value (From Top Left), Y Value (From Top Left), Width, Height (ALL IN PIXELS)
+        loadButton.setBounds(20, 50, 100, 40); // IN ORDER: X Value (From Top Left), Y Value (From Top Left), Width, Height (ALL IN PIXELS)
         frame.getContentPane().add(loadButton); // Add loadButton to the JFrame
 
         JButton saveButton = new JButton("Save Game"); // Make the saveButton
-        saveButton.setBounds(20,100,100,40); // IN ORDER: X Value (From Top Left), Y Value (From Top Left), Width, Height (ALL IN PIXELS)
+        saveButton.setBounds(20, 100, 100, 40); // IN ORDER: X Value (From Top Left), Y Value (From Top Left), Width, Height (ALL IN PIXELS)
         frame.getContentPane().add(saveButton); // Add saveButton to the JFrame
+
+        JTextArea outputArea = new JTextArea();
+        outputArea.setBounds(140, 50, 1090, 620);
+        frame.getContentPane().add(outputArea);
         
         frame.setSize(1280, 720); // Set the size of the window when opened
         frame.setLayout(null); // Set the window layout
