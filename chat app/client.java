a577844a4939d5f469b881550763788fce49e614
mimramon <alonsosanzmm@gmail.com>
2021-05-22 23:44:56 +0100

    server to client works but not vice versa

--- chat app/client.java
+++ chat app/client.java
@@ -3,22 +3,29 @@ import java.net.*;
 
 public class client
 {
-
     public Socket socket;
     public DataInputStream inStream;
     public DataOutputStream outStream;
 
     public client() throws IOException
     {
-        GUI gui = new GUI(this);
+        clientGUI gui = new clientGUI(this);
         socket = new Socket("localhost", 4999);
         inStream = new DataInputStream(socket.getInputStream());
         outStream = new DataOutputStream(socket.getOutputStream());
+       
         while(true)
         {
             String incomingMessage = inStream.readUTF();
             gui.textArea.append("server says: " + incomingMessage + "\n");
         }
+    }
+
+    public void sendMessage(String input) throws IOException
+    {
+        outStream = new DataOutputStream(socket.getOutputStream());
+        outStream.writeUTF(input);
+        outStream.flush();
     }
 
     public static void main(String[] args) throws IOException
