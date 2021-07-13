8b8412d520f6ec574eeffd31f3bd6047018fdfee
Miguel Martinez <37045227+saiyanninja@users.noreply.github.com>
2021-07-13 15:19:17 +0100

    started rocket sim

---
+++ RocketSimulation/Main.java
@@ -0,0 +1,17 @@
+class Main
+{
+    public static void main(String[] args) 
+    {
+        while(true)
+        {
+            force();
+        }    
+    }
+
+    public double[] force()
+    {
+        double force[] = new double[3];
+        gravForce = (G*mass1*mass2)/dist;
+        return force;
+    }
+}
